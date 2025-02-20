d2970881357f64758a0c169224688c58e56fea99
Tom Cavilla <46577288+TMC330@users.noreply.github.com>
2025-02-20 19:21:41 +0000

    build config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +12,18 @@ export default defineConfig({
   server: {
     open: true,
     port: 4000
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'events/index.html'),
+      },
+    },
+  },
 })
+
+
+
+
+
