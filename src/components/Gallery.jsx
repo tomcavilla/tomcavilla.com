d0746d59bea5ea33883115c9c0dbe406e25210f7
Tom Cavilla <46577288+TMC330@users.noreply.github.com>
2025-02-21 12:37:34 +0000

    mistake

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,7 @@
 const Gallery = () => {
     return (
-      <section id="gallery" className="py-16 bg-stone-50">
-        <h2 className="text-3xl ml-6 mr-6 font-bold text-center">GALLERIES</h2>
+      <section id="gallery" className="py-16 bg-stone-50 ml-6 mr-6">
+        <h2 className="text-3xl font-bold text-center">GALLERIES</h2>
         <div className="mt-6 flex justify-center space-x-4">
           <a href="/gallery/"><img src="https://lh3.googleusercontent.com/pw/AP1GczP07WhyWY7GXJLuWfOUZh-WvNGY2iYqo1oVonyCh49s_zY0xfHJYdrCXGy8JCRp12A7W2Fn6641SPNBelc6_DPU2tpgdZzsoNOLck-jbneMw04WL8aSbVTY98D1FythWcTD6P-wh0eMeNlCEAiX_wHS=w872-h581-s-no-gm?authuser=0" alt="Bristol " className="rounded-lg shadow-md"/></a>
           <a href="/gallery/"><img src="https://lh3.googleusercontent.com/pw/AP1GczPqqmiNx6ACK_85SruQXYgnn1ch0MlP2xeqiXUaB_IqjlGA7KeGy5KYLo9u3jWSKlSQ6MSdj15NNunIq5hMYIyf6nPX2plTZJinizaRHjCNsXQb04dySAsfY6bAbZtsufPEFps7Me1Kql0QVP7AIfdo=w872-h581-s-no-gm?authuser=0" alt="Gallery 2" className="rounded-lg shadow-md"/></a>
