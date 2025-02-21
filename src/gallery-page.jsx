56abad9aeda2569c0bbe35b0694610ed058d2618
Tom Cavilla <46577288+TMC330@users.noreply.github.com>
2025-02-21 14:58:45 +0000

    Exmouth

--- src/gallery-page.jsx
+++ src/gallery-page.jsx
@@ -10,9 +10,20 @@ const Gallery = () => {
                     <h1 class="text-7xl">Gallery</h1>
                     <p class="pt-4">Collections of the rest of my photography work.</p>
                     <section>
+                        <div id="Exmouth09-02">
+                            <a href="/exmouth/"><h3 class="gallery-link text-2xl underline pt-8">Exmouth 09/02/2025</h3></a>
+                            <p class="pt-2">Finally a bit of sun came out in February and had to get out. Photos from a walk around Exmouth, shot Sony A7IV - 50mm f/1.8 Sony Lens.</p>
+                            <div class="grid grid-cols-3 gap-4 m-auto justify-items-center pt-8">
+                                <div class="w-1/2"><img src="https://lh3.googleusercontent.com/pw/AP1GczMxQQq6YUzYxo-EkHbevXbxM7ueba_BEs75n145IpFGcjAcPNZgMDW0wpjyryGAsYnTsCMyoVe2sfURElUW_9iCMBwIk25LXXvt5XFJmsGoAMCMuzLa2ec0qBn83KpCd7zsQYX16OvwN549uVZ-G7k6=w1554-h1036-s-no-gm?authuser=0"></img></div>
+                                <div class="w-1/2"><img src="https://lh3.googleusercontent.com/pw/AP1GczN_4EDAUDY_pa3j5AipuLD0ZiRKn-lMm4HN3kQkIn2bWMf9ULfjCdVShcEajD7AdUwbUOjHCbe8TEbLztNOBHx_qM5M1ECPiyY1BREFjmd8nr0QYInUkepJJlTWnuuWEVdk0Ywh_V7jLM4jojEPqd4K=w1554-h1036-s-no-gm?authuser=0"></img></div>
+                                <div class="w-1/2"><img src="https://lh3.googleusercontent.com/pw/AP1GczNY3cBtKwZeZsaMbaL2zYMXmZ_FFpNo--p-y2IwIfXu2qYKU3Qg7NRlyKjrVfV5Ai4xndvHpUXUx3sTiMvgmtdCNTnrlLviblVzYRr_k3C_YVhiW5iaFYP50slVhdPkwE9xuCpqP6Tun2y2Aeslw1vr=w1554-h1036-s-no-gm?authuser=0"></img></div>
+                            </div>
+                        </div>
+                    </section>
+                    <section>
                         <div id="Bristol09-01">
                             <a href='/bristol/'><h3 class="gallery-link text-2xl underline pt-8">Bristol 09/01/2025</h3></a>
-                            <p class="pt-2">Day trip to Bristol in January 2025.</p>
+                            <p class="pt-2">Day trip to Bristol in January 2025. Shot on Fujifilm x100F.</p>
                             <div class="grid grid-cols-2 gap-4 m-auto justify-items-center pt-8">
                                 <div class="grid gap-4 justify-items-end">
                                 <div class="w-1/2"><img src="https://lh3.googleusercontent.com/pw/AP1GczP07WhyWY7GXJLuWfOUZh-WvNGY2iYqo1oVonyCh49s_zY0xfHJYdrCXGy8JCRp12A7W2Fn6641SPNBelc6_DPU2tpgdZzsoNOLck-jbneMw04WL8aSbVTY98D1FythWcTD6P-wh0eMeNlCEAiX_wHS=w872-h581-s-no-gm?authuser=0"></img></div>
@@ -24,6 +35,7 @@ const Gallery = () => {
                             </div>
                         </div>
                     </section>
+                    
                 </section>
             </div>
         </div>
