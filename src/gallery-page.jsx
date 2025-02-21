bb6f63ba8c9266333ffbf46f61163af8a3f59bc1
Tom Cavilla <46577288+TMC330@users.noreply.github.com>
2025-02-21 14:46:14 +0000

    padding

--- src/gallery-page.jsx
+++ src/gallery-page.jsx
@@ -8,12 +8,12 @@ const Gallery = () => {
             <div class="py-16 text-center">
                 <section id="Galleries" class="">
                     <h1 class="text-7xl">Gallery</h1>
-                    <p class="pt-8">Collections of the rest of my photography work.</p>
+                    <p class="pt-4">Collections of the rest of my photography work.</p>
                     <section>
                         <div id="Bristol09-01">
-                            <a href='/bristol/'><h3 class="gallery-link text-2xl underline pt-16">Bristol 09/01/2025</h3></a>
-                            <p class="pt-8">Day trip to Bristol in January 2025.</p>
-                            <div class="grid grid-cols-2 gap-4 m-auto justify-items-center pt-16">
+                            <a href='/bristol/'><h3 class="gallery-link text-2xl underline pt-8">Bristol 09/01/2025</h3></a>
+                            <p class="pt-2">Day trip to Bristol in January 2025.</p>
+                            <div class="grid grid-cols-2 gap-4 m-auto justify-items-center pt-8">
                                 <div class="grid gap-4 justify-items-end">
                                 <div class="w-1/2"><img src="https://lh3.googleusercontent.com/pw/AP1GczP07WhyWY7GXJLuWfOUZh-WvNGY2iYqo1oVonyCh49s_zY0xfHJYdrCXGy8JCRp12A7W2Fn6641SPNBelc6_DPU2tpgdZzsoNOLck-jbneMw04WL8aSbVTY98D1FythWcTD6P-wh0eMeNlCEAiX_wHS=w872-h581-s-no-gm?authuser=0"></img></div>
                                 <div class="w-1/2"><img src="https://lh3.googleusercontent.com/pw/AP1GczNPScLLY2stblXJ-hCD5t2YQQj_fHwtj0YI5pYK67ZG2WGqB7FlGwZzVxE1_v_uFouSqAhje5lNeiuYgfPz28J3mb5IHw3CheULhOyiXnBMrxRHKPhPV0IGZ-KuyQ1iM2mO1sEHM99YNrXu4r0I3Ytn=w1554-h1036-s-no-gm?authuser=0"></img></div>
