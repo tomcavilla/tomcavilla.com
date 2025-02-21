70ab61396dba91fc62b3ed2204c5d4f7674a1eee
Tom Cavilla <tmcavilla@gmail.com>
2025-02-21 16:11:57 +0000

    Remove button, make title the link, and image clickable

--- src/gallery-page.jsx
+++ src/gallery-page.jsx
@@ -8,21 +8,25 @@ const Gallery = () => {
           title: "Exmouth 09/02/25",
           description: "Finally a bit of sun came out in February and had to get out. Photos from a walk around Exmouth, shot Sony A7IV - 50mm f/1.8 Sony Lens.",
           image: "https://lh3.googleusercontent.com/pw/AP1GczN_4EDAUDY_pa3j5AipuLD0ZiRKn-lMm4HN3kQkIn2bWMf9ULfjCdVShcEajD7AdUwbUOjHCbe8TEbLztNOBHx_qM5M1ECPiyY1BREFjmd8nr0QYInUkepJJlTWnuuWEVdk0Ywh_V7jLM4jojEPqd4K=w1554-h1036-s-no-gm?authuser=0",
+          link: "/exmouth/",
         },
         {
           title: "",
           description: "",
           image: "",
+          link: "",
         },
         {
           title: "",
           description: "",
           image: "",
+          link: "",
         },
         {
           title: "Bristol 09/01/25",
           description: "Day trip to Bristol in January 2025. Shot on Fujifilm x100F.",
           image: "https://lh3.googleusercontent.com/pw/AP1GczP07WhyWY7GXJLuWfOUZh-WvNGY2iYqo1oVonyCh49s_zY0xfHJYdrCXGy8JCRp12A7W2Fn6641SPNBelc6_DPU2tpgdZzsoNOLck-jbneMw04WL8aSbVTY98D1FythWcTD6P-wh0eMeNlCEAiX_wHS=w872-h581-s-no-gm?authuser=0https://lh3.googleusercontent.com/pw/AP1GczP07WhyWY7GXJLuWfOUZh-WvNGY2iYqo1oVonyCh49s_zY0xfHJYdrCXGy8JCRp12A7W2Fn6641SPNBelc6_DPU2tpgdZzsoNOLck-jbneMw04WL8aSbVTY98D1FythWcTD6P-wh0eMeNlCEAiX_wHS=w872-h581-s-no-gm?authuser=0",
+          link: "/bristol/",
         },
     ];
     
@@ -31,19 +35,17 @@ const Gallery = () => {
             <Navbar />
             <div class="py-16 text-center justify-items-center">
                 <section id="Galleries" class="">
-                    <div class="w-1/3 mx-auto text-center">
                     <h1 class="text-7xl">Gallery</h1>
                     <p class="pt-4">Collections of the rest of my photography.</p>
-                    </div>
                     <div className="max-w-6xl mx-auto p-6">
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                             {projects.map((project, index) => (
                             <div key={index} className="flex flex-col md:flex-row items-center gap-6">
-                                <img src={project.image} alt={project.title} className="w-full md:w-1/2 aspect-[16/9] object-cover rounded-lg shadow-md" />
+                                <a href={project.link}><img src={project.image} alt={project.title} className="w-full md:w-3/4 aspect-[16/9] object-cover rounded-lg shadow-md" /></a>
                                 <div className="md:w-1/2">
-                                <h2 className="text-2xl font-bold">{project.title}</h2>
+                                <a href={project.link}><h2 className="text-2xl font-bold gallery-link">{project.title}</h2></a>
                                 <p className="text-gray-600 mt-2">{project.description}</p>
-                                <button className="mt-4 px-4 py-2 bg-black text-white rounded-md">See more</button>
+
                                 </div>
                             </div>
                             ))}
