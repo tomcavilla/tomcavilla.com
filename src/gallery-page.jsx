5f7246df7e5aa28f854a51078a14addeab4fa3f0
Tom Cavilla <46577288+TMC330@users.noreply.github.com>
2025-02-21 12:58:16 +0000

    gallery page creation

---
+++ src/gallery-page.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+const Gallery = () => {
+    return (
+        <div>
+            <Navbar />
+            <div class="py-16">
+                <section id="Galleries" class="">
+                    <h1 class="text-7xl">Gallery</h1>
+                    <p>Collections of the rest of my photography work.</p>
+                    <section>
+                        <div id="Bristol09-01">
+                            <a href='/bristol/'><h3 class="text-2xl underline hover:bg-stone-300">Bristol 09/01/2025</h3></a>
+                            <p>Day trip to Bristol in January 2025.</p>
+                            <div class="grid grid-cols-3 gap-4">
+                                <img src=""></img>
+                                <img src=""></img>
+                                <img src=""></img>
+                            </div>
+                        </div>
+                    </section>
+                </section>
+            </div>
+        </div>
+    );
+};
+
+
+
+
+export default Gallery;
