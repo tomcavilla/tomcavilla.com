f4fa9d6abc125f05a0b505b57902366b1427b002
Tom Cavilla <46577288+TMC330@users.noreply.github.com>
2025-02-21 14:06:06 +0000

    Check

--- src/gallery-page.jsx
+++ src/gallery-page.jsx
@@ -15,8 +15,8 @@ const Gallery = () => {
                             <p>Day trip to Bristol in January 2025.</p>
                             <div class="grid grid-cols-2 gap-4">
                                 <div class="grid gap-4">
-                                <div class="h-1/4"><img src="https://lh3.googleusercontent.com/pw/AP1GczP07WhyWY7GXJLuWfOUZh-WvNGY2iYqo1oVonyCh49s_zY0xfHJYdrCXGy8JCRp12A7W2Fn6641SPNBelc6_DPU2tpgdZzsoNOLck-jbneMw04WL8aSbVTY98D1FythWcTD6P-wh0eMeNlCEAiX_wHS=w872-h581-s-no-gm?authuser=0"></img></div>
-                                <div class="h-1/4"><img src="https://lh3.googleusercontent.com/pw/AP1GczNPScLLY2stblXJ-hCD5t2YQQj_fHwtj0YI5pYK67ZG2WGqB7FlGwZzVxE1_v_uFouSqAhje5lNeiuYgfPz28J3mb5IHw3CheULhOyiXnBMrxRHKPhPV0IGZ-KuyQ1iM2mO1sEHM99YNrXu4r0I3Ytn=w1554-h1036-s-no-gm?authuser=0"></img></div>
+                                <div class="w-1/4"><img src="https://lh3.googleusercontent.com/pw/AP1GczP07WhyWY7GXJLuWfOUZh-WvNGY2iYqo1oVonyCh49s_zY0xfHJYdrCXGy8JCRp12A7W2Fn6641SPNBelc6_DPU2tpgdZzsoNOLck-jbneMw04WL8aSbVTY98D1FythWcTD6P-wh0eMeNlCEAiX_wHS=w872-h581-s-no-gm?authuser=0"></img></div>
+                                <div class="w-1/4"><img src="https://lh3.googleusercontent.com/pw/AP1GczNPScLLY2stblXJ-hCD5t2YQQj_fHwtj0YI5pYK67ZG2WGqB7FlGwZzVxE1_v_uFouSqAhje5lNeiuYgfPz28J3mb5IHw3CheULhOyiXnBMrxRHKPhPV0IGZ-KuyQ1iM2mO1sEHM99YNrXu4r0I3Ytn=w1554-h1036-s-no-gm?authuser=0"></img></div>
                                 </div>
                                 <div class ="grid gap-4">
                                 <div class="w-1/4"><img src="https://lh3.googleusercontent.com/pw/AP1GczMaENFfQuFhq6vIdk0pHtBL-8X4TwZsF79RfejL_XUEgdso2MswWV2LqAu-Tsu337kDmwuZ-HBbm_CuKR-gWvIwCf0DZ7HMfzj5DdfXuxsKGgADa0-DFulvnoOC67LdLsW4H-pF-x44P_tJ6lDA36tg=w691-h1036-s-no-gm?authuser=0"></img></div>
